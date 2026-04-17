d3547b07f518e2c9c55bee20133c312136b8b834
Ahmed Abdelsalam <firepanda976@gmail.com>
2026-04-17 21:49:00 +0200

    Basis of basis

---
+++ src/main/java/com/example/osproject/Process.java
@@ -0,0 +1,5 @@
+package com.example.osproject;
+
+public class Process {
+    int waitingtime,responsetime,turnaroundtime,arrivaltime;
+}
---
+++ src/main/java/com/example/osproject/RR.java
@@ -0,0 +1,28 @@
+package com.example.osproject;
+
+public class RR {
+    int Processcount,quantum;
+    int [] processes;
+
+
+
+    public RR(int processcount, int[] processes, int quantum) {
+        Processcount = processcount;
+        this.processes = processes;
+        this.quantum = quantum;
+    }
+
+
+    public averagewaitingtime(){
+        //todo(Calculate average waiting time)
+
+    }
+
+    public singlewaitingtime(Process p){
+
+    }
+
+
+
+}
+
