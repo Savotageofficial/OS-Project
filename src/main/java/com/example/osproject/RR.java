75f1888a5948b1eb7a9a58922f7b455fb27359f7
Ahmed Abdelsalam <firepanda976@gmail.com>
2026-04-24 18:09:51 +0300

    Coding The Round Robin Engine

--- src/main/java/com/example/osproject/RR.java
+++ src/main/java/com/example/osproject/RR.java
@@ -14,7 +14,6 @@ public class RR {
         // See What java gonna compare between objects
         Arrays.sort(processes , Comparator.comparingInt(p -> p.arrivaltime));
 
-
         int time = 0;
         int completed = 0;
 
@@ -27,6 +26,54 @@ public class RR {
             }
         }
 
+        while(completed < n) {
+            while (!(ready.isEmpty())) {
+
+                process p = ready.poll();
+//                Checking the ready
+                if (p.remainingtime > q) {
+                    //execute the process completely in its time quantum
+                    p.remainingtime -= q;
+                    time += q;
+                    //check what processes arrived , aka what processes have arrival time = time and add them to arrived
+                    for (int i = 0; i < n; i++) {
+                        if (((processes[i].arrivaltime > (time - q)) && (processes[i].arrivaltime <= time)) && (!ready.contains(processes[i])) && (!processes[i].completed)) {
+                            ready.add(processes[i]);
+                        }
+                    }
+                    //insert p at the end of the queue
+                    ready.add(p);
+                } else if (p.remainingtime <= q) {
+                    //execute p for its remaining time
+                    time += p.remainingtime;
+                    p.completiontime = time;
+                    p.turnaroundtime = time - p.arrivaltime;
+                    p.waitingtime = p.turnaroundtime - p.bursttime;
+                    p.remainingtime = 0;
+                    p.completed = true;
+                    completed++;
+
+                    //check what processes arrived , aka what processes have arrival time = time and add them to arrived
+                    for (int i = 0; i < n; i++) {
+                        if (((processes[i].arrivaltime > (time - q)) && (processes[i].arrivaltime <= time)) && (!ready.contains(processes[i])) && (!processes[i].completed)) {
+                            ready.add(processes[i]);
+                        }
+                    }
+
+                }
+            }
+            if (completed < n) {
+                time = processes[completed].arrivaltime;
+
+                for (int i = 0; i < n; i++) {
+                    if (processes[i].arrivaltime <= time && !processes[i].completed && !ready.contains(processes[i])) {
+                        ready.add(processes[i]);
+                    }
+                }
+
+
+            }
+        }
 
     }
 
