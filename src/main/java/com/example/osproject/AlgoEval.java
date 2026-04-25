2a90a788ee48405977af19ba06106ae9d98b861b
Ahmed Abdelsalam <firepanda976@gmail.com>
2026-04-25 18:51:24 +0300

    Algo Eval Base

--- src/main/java/com/example/osproject/AlgoEval.java
+++ src/main/java/com/example/osproject/AlgoEval.java
@@ -1,4 +1,51 @@
 package com.example.osproject;
 
+import java.util.*;
+
 public class AlgoEval {
+
+    private process[] rrProcesses;
+    private process[] srtfProcesses;
+    private int quantum;
+
+    // Averages for RR
+    private double rrAvgWT, rrAvgTAT, rrAvgRT;
+    // Averages for SRTF
+    private double srtfAvgWT, srtfAvgTAT, srtfAvgRT;
+
+    public AlgoEval(List<process> originalProcesses, int quantum) {
+        this.quantum = quantum;
+
+        // Calculate averages
+        rrAvgWT = avgWT(rrProcesses);
+        rrAvgTAT = avgTAT(rrProcesses);
+        rrAvgRT = avgRT(rrProcesses);
+
+        srtfAvgWT = avgWT(srtfProcesses);
+        srtfAvgTAT = avgTAT(srtfProcesses);
+        srtfAvgRT = avgRT(srtfProcesses);
+    }
+
+    // Averages calculators
+
+    private double avgWT(process[] procs) {
+        double sum = 0;
+        for (process p : procs)
+            sum += p.waitingtime;
+        return sum / procs.length;
+    }
+
+    private double avgTAT(process[] procs) {
+        double sum = 0;
+        for (process p : procs)
+            sum += p.turnaroundtime;
+        return sum / procs.length;
+    }
+
+    private double avgRT(process[] procs) {
+        double sum = 0;
+        for (process p : procs)
+            sum += p.responsetime;
+        return sum / procs.length;
+    }
 }
