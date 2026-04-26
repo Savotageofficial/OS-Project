06f5e334f9334f32851501b4ab27fdc971674a53
adham <adham.mohamedgad.06@gmail.com>
2026-04-26 16:05:59 +0300

    Added all getters

--- src/main/java/com/example/osproject/process.java
+++ src/main/java/com/example/osproject/process.java
@@ -12,41 +12,59 @@ package com.example.osproject;
         boolean started;
         boolean completed;
 
-    public process(int pid ,int arrivaltime , int bursttime) {
-        this.pid = pid;
-        this.arrivaltime = arrivaltime;
-        this.bursttime = bursttime;
-        this.remainingtime = bursttime;
-        this.responsetime = -1;
-        this.started = false;
-        this.completed = false;
+        public process(int pid, int arrivaltime, int bursttime) {
+            this.pid = pid;
+            this.arrivaltime = arrivaltime;
+            this.bursttime = bursttime;
+            this.remainingtime = bursttime;
+            this.responsetime = -1;
+            this.started = false;
+            this.completed = false;
+        }
+
+        public void reset() {
+            remainingtime = bursttime;
+            waitingtime = 0;
+            turnaroundtime = 0;
+            completiontime = 0;
+            responsetime = -1;
+            started = false;
+            completed = false;
+        }
+
+        public process copy() {
+
+            return new process(pid, arrivaltime, bursttime);
+
+        }
+        public int pid(){
+            return pid;
+        }
+        public int getArrivaltime(){
+            return arrivaltime;
+
+        }
+        public int getBursttime(){
+            return bursttime;
+        }
+        public int getRemainingtime(){
+            return remainingtime;
+        }
+        public int getWaitingtime(){
+            return waitingtime;
+        }
+        public int getTurnaroundtime(){
+            return turnaroundtime;
+        }
+        public int getResponsetime(){
+            return responsetime;
+        }
+        public int getCompletiontime(){
+            return completiontime;
+        }
+        public boolean isCompleted(){
+            return completed;
+        }
+
+
     }
-
-    public void reset(){
-        remainingtime = bursttime;
-        waitingtime = 0;
-        turnaroundtime = 0;
-        completiontime = 0;
-        responsetime = -1;
-        started = false;
-        completed = false;
-    }
-    public process copy(){
-
-        return new process(pid,arrivaltime,bursttime);
-
-    }
-
-
-    public int getBursttime() {
-        return bursttime;
-    }
-
-    public int getArrivaltime() {
-        return arrivaltime;
-    }
-
-    public int getWaitingtime() {
-        return waitingtime;
-    }
-}
