4775df2d3b70068e05b4c6eab1cadc82a109ed52
palmhillstry <mohamedgamesar4321@gmail.com>
2026-04-27 20:13:27 +0300

    fixed some syntax bugs in process.java

--- src/main/java/com/example/osproject/process.java
+++ src/main/java/com/example/osproject/process.java
@@ -1,45 +1,40 @@
 package com.example.osproject;
 
-    public class process {
-        int pid;
-        int arrivaltime;
-        int bursttime;
-        int remainingtime;
-        int waitingtime;
-        int turnaroundtime;
-        int completiontime;
-        int responsetime;
-        boolean started;
-        boolean completed;
+public class process {
+    int pid;
+    int arrivaltime;
+    int bursttime;
+    int remainingtime;
+    int waitingtime;
+    int turnaroundtime;
+    int completiontime;
+    int responsetime;
+    boolean started;
+    boolean completed;
 
-        public process(int pid, int arrivaltime, int bursttime) {
-            this.pid = pid;
-            this.arrivaltime = arrivaltime;
-            this.bursttime = bursttime;
-            this.remainingtime = bursttime;
-            this.responsetime = -1;
-            this.started = false;
-            this.completed = false;
-        }
-
-        public void reset() {
-            remainingtime = bursttime;
-            waitingtime = 0;
-            turnaroundtime = 0;
-            completiontime = 0;
-            responsetime = -1;
-            started = false;
-            completed = false;
-        }
-
-        public process copy() {
-            return new process(this.pid, this.arrivaltime, this.bursttime);
-        }
-
-    public int getBursttime() {
-        return bursttime;
+    public process(int pid, int arrivaltime, int bursttime) {
+        this.pid = pid;
+        this.arrivaltime = arrivaltime;
+        this.bursttime = bursttime;
+        this.remainingtime = bursttime;
+        this.responsetime = -1;
+        this.started = false;
+        this.completed = false;
     }
 
+    public void reset() {
+        remainingtime = bursttime;
+        waitingtime = 0;
+        turnaroundtime = 0;
+        completiontime = 0;
+        responsetime = -1;
+        started = false;
+        completed = false;
+    }
+
+    public process copy() {
+        return new process(this.pid, this.arrivaltime, this.bursttime);
+    }
 
     public int pid(){
         return pid;
