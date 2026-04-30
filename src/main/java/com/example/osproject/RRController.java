906c6ac8a294340a559bae4604a000ed415b68be
XXMennahMahmoudXX <mennahmahmoud398@gmail.com>
2026-04-30 17:21:03 +0300

    Modifying the basics of the view, writing a function to load data, and creating the CSS file

--- src/main/java/com/example/osproject/RRController.java
+++ src/main/java/com/example/osproject/RRController.java
@@ -1,4 +1,42 @@
 package com.example.osproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import java.util.List;
+
 public class RRController {
+
+        @FXML
+        private HBox ganttBox;
+
+        @FXML
+        private TableView<process> tableView;
+
+        @FXML
+        private Label avgWTLabel, avgTATLabel, avgRTLabel;
+
+public void loadData(List<process> processes, int quantum) {
+            AlgoEval algoEval = new AlgoEval(processes, quantum);
+            process[] rr = algoEval.getRrProcesses();
+
+
+            tableView.getItems().addAll(rr);
+
+
+            avgWTLabel.setText("Avg WT: " + algoEval.getRrAvgWT());
+            avgTATLabel.setText("Avg TAT: " + algoEval.getRrAvgTAT());
+            avgRTLabel.setText("Avg RT: " + algoEval.getRrAvgRT());
+        }
+
+
 }
+
+
+
+
+
+
+
+
