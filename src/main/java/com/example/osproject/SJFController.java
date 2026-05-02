a36b4c7287b40d4d584fc893fb5c663606a21317
JANA <janapola115@gmail.com>
2026-05-03 02:56:23 +0300

    connect table columns with data

--- src/main/java/com/example/osproject/SJFController.java
+++ src/main/java/com/example/osproject/SJFController.java
@@ -1,4 +1,37 @@
 package com.example.osproject;
+import com.example.osproject.SJF;
+import com.example.osproject.process;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.collections.*;
 
-public class SJFController {
+public class  SJFController {
+    @FXML private TableView<process> table;
+    @FXML private TableColumn<process , Integer> pidCol;
+    @FXML private TableColumn<process , Integer> wtCol;
+    @FXML private TableColumn<process , Integer> tatCol;
+    @FXML private TableColumn<process , Integer> rtCol;
+
+    @FXML private HBox ganttBox;
+
+    @FXML private Label avgWT;
+    @FXML private Label avgTAT;
+    @FXML private Label avgRT;
+    @FXML public void initialize(){
+        pidCol.setCellValueFactory(new PropertyValueFactory<>("pid"));
+        pidCol.setCellValueFactory(new PropertyValueFactory<>("waitingTime"));
+        pidCol.setCellValueFactory(new PropertyValueFactory<>("turnaroundTime"));
+        pidCol.setCellValueFactory(new PropertyValueFactory<>("responseTime"));
+    }
+
+
+
+
+
+
+
 }
+
+
