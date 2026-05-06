a1878678a250134138204696b8afc74d0649ac6e
Ahmed Abdelsalam <firepanda976@gmail.com>
2026-05-06 18:33:03 +0300

    Edits for error avoiding

--- src/main/java/com/example/osproject/RR.java
+++ src/main/java/com/example/osproject/RR.java
@@ -5,9 +5,16 @@ import java.util.*;
 public class RR {
     int Processcount, quantum;
     int[] processes;
+    private static List <process> executionOrder = new ArrayList<>();
+    private static HashMap<Integer, Integer> executionDurations = new HashMap<>();
+    private static HashMap<Integer, process> executionTimeline = new HashMap<>();
+
 
     public static void RR(process[] processes, int q,HashMap<Integer, Queue> RQs) {
         int n = processes.length;
+        executionOrder.clear();
+        executionDurations.clear();
+        executionTimeline.clear();
 
         // See What java gonna compare between objects
         Arrays.sort(processes, Comparator.comparingInt(p -> p.arrivaltime));
@@ -33,6 +40,9 @@ public class RR {
                 if (p.remainingtime > q) {
                     // execute the process completely in its time quantum
                     p.remainingtime -= q;
+                    executionOrder.add(p);
+                    executionTimeline.put(time, p);
+                    executionDurations.put(time, q);
                     time += q;
                     // check what processes arrived , aka what processes have arrival time = time
                     // and add them to arrived
@@ -46,6 +56,9 @@ public class RR {
                     ready.add(p);
                 } else if (p.remainingtime <= q) {
                     // execute p for its remaining time
+                    executionOrder.add(p);
+                    executionTimeline.put(time, p);
+                    executionDurations.put(time, p.remainingtime);
                     time += p.remainingtime;
                     p.completiontime = time;
                     p.turnaroundtime = time - p.arrivaltime;
@@ -79,6 +92,16 @@ public class RR {
 
 
 
+    }
+    public HashMap<Integer, Integer> getExecutionDurations() {
+        return executionDurations;
+    }
+
+    public HashMap<Integer, process> getExecutionTimeline() {
+        return executionTimeline;
+    }
+    public List<process> getExecutionOrder() {
+        return executionOrder;
     }
 
 }
