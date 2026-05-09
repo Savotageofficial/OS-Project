e416b59d8d35879ab38418c76f77680d28033957
XXMennahMahmoudXX <mennahmahmoud398@gmail.com>
2026-05-10 00:55:42 +0300

    Edit logout function

--- src/main/java/com/example/osproject/Main.java
+++ src/main/java/com/example/osproject/Main.java
@@ -2,12 +2,13 @@ package com.example.osproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -35,16 +36,51 @@ public class Main extends Application {
             logOut(stage);
         });
     }
-    public void logOut (Stage stage){
-        Alert alert = new Alert(Alert.AlertType.WARNING);
+    public void logOut(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.NONE);
+        Stage alertStage = (Stage)alert.getDialogPane().getScene().getWindow();
+        ImageView image = new ImageView(new Image("logout.png"));
+        ButtonType yesSaveButton = new ButtonType("Save");
+        ButtonType dontSaveButton = new ButtonType("Don't Save");
+        ButtonType cancelButton = new ButtonType("Cancel");
+        alertStage.getIcons().add(new Image("logout2.png"));
 
         alert.setTitle("Logout");
-        alert.setHeaderText("you're about logout!");
-        alert.setContentText("do you want to save before exiting?");
-        alert.getButtonTypes().addAll( ButtonType.CANCEL);
+        alert.setHeaderText("   You're about to logout!");
+        alert.setGraphic(image);
+        alert.getButtonTypes().setAll(yesSaveButton, dontSaveButton, cancelButton);
 
-        if (alert.showAndWait().get() == ButtonType.OK) {
-            System.out.println("you successfully logged out!");
+        Button yesButton = (Button)alert.getDialogPane().lookupButton(yesSaveButton);
+        Button dontSaveBtn = (Button)alert.getDialogPane().lookupButton(dontSaveButton);
+        Button cancelBtn = (Button)alert.getDialogPane().lookupButton(cancelButton);
+        yesButton.setId("yesBtn");
+        dontSaveBtn.setId("dontSaveBtn");
+        cancelBtn.setId("cancelBtn");
+
+        alertStage.setWidth(360);
+        alertStage.setHeight(100);
+
+        image.setFitWidth(120);
+        image.setFitHeight(120);
+        image.setTranslateX(-10);
+        image.setTranslateY(40);
+
+        yesButton.setTranslateX(-70);
+        yesButton.setTranslateY(-90);
+        dontSaveBtn.setTranslateX(-70);
+        dontSaveBtn.setTranslateY(-90);
+        cancelBtn.setTranslateX(-205);
+        cancelBtn.setTranslateY(-50);
+
+        alert.getDialogPane().getStylesheets().add(getClass().getResource("logout.css").toExternalForm());
+        ButtonType result =alert.showAndWait().orElse(cancelButton);
+        if (result == yesSaveButton) {
+            controller.saveData();
+            System.out.println("Saved!");
+            stage.close();
+        }
+        else if (result == dontSaveButton) {
+            System.out.println("Closed without saving");
             stage.close();
         }
     }
